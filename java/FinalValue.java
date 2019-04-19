7f64c67626d431df5479fce89d9c3a90b71f6cb5
yangdx101 <37408640+yangdx101@users.noreply.github.com>
2019-04-19 23:53:38 +0800

    finalvalue

---
+++ java/FinalValue.java
@@ -0,0 +1,11 @@
+package util;
+
+import java.text.SimpleDateFormat;
+
+public class FinalValue {
+	public final static String DATETIMEFORMAT = "yyyy-MM-dd HH:mm:ss";
+	public final static SimpleDateFormat SDF = new SimpleDateFormat(DATETIMEFORMAT);
+	public final static String ISO88591 = "ISO-8859-1";
+	public final static String UTF8 = "UTF-8";
+	public final static String GBK = "GBK";
+}
